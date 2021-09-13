d640ae2083c83c36d3e2e8a1252be887d7921bde
Vlad D <vlad.drahnea@gmail.com>
2021-09-13 16:00:04 +0300

    move uuser defaults keys to an enum

--- beeper/Services/UDService.swift
+++ beeper/Services/UDService.swift
@@ -7,20 +7,24 @@
 
 import Foundation
 
-let usernameKey = "usernamekey"
-let passwordKey = "passwordkey"
-let hostKey = "hostkey"
-let portKey = "portkey"
+
+enum StorageKeys: String {
+    case username = "usernamekey"
+    case password = "passwordkey"
+    case host = "hostkey"
+    case port = "portkey"
+    case soundboards = "soundboardskey"
+}
 
 class UDService {
     static let shared = UDService()
     
 //    MARK: Connections
     func save(Connection connection: Connection) -> Void {
-        UserDefaults.standard.register(defaults: [hostKey: connection.host])
-        UserDefaults.standard.register(defaults: [portKey: connection.port])
-        UserDefaults.standard.register(defaults: [usernameKey: connection.username])
-        UserDefaults.standard.register(defaults: [passwordKey: connection.password])
+        UserDefaults.standard.register(defaults: [StorageKeys.host.rawValue: connection.host])
+        UserDefaults.standard.register(defaults: [StorageKeys.port.rawValue: connection.port])
+        UserDefaults.standard.register(defaults: [StorageKeys.username.rawValue: connection.username])
+        UserDefaults.standard.register(defaults: [StorageKeys.password.rawValue: connection.password])
     }
     
     func eraseConnectionData() -> Void {
@@ -28,22 +32,22 @@ class UDService {
     }
     
     func getConnection() -> Connection? {
-        let username = UserDefaults.standard.value(forKey: usernameKey) as! String?
+        let username = UserDefaults.standard.value(forKey: StorageKeys.username.rawValue) as! String?
         if username == nil {
             return nil
         }
         
-        let password = UserDefaults.standard.value(forKey: passwordKey) as! String?
+        let password = UserDefaults.standard.value(forKey: StorageKeys.password.rawValue) as! String?
         if password == nil {
             return nil
         }
         
-        let host = UserDefaults.standard.value(forKey: hostKey) as! String?
+        let host = UserDefaults.standard.value(forKey: StorageKeys.host.rawValue) as! String?
         if host == nil {
             return nil
         }
         
-        let port = UserDefaults.standard.value(forKey: portKey) as! UInt16?
+        let port = UserDefaults.standard.value(forKey: StorageKeys.port.rawValue) as! UInt16?
         if port == nil {
             return nil
         }
